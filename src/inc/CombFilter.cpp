b14188af117f4eeec9e5ead7dcf8331143c84631
johnnymac647 <jmcnamara7@gatech.edu>
2022-02-10 22:17:26 -0500

    added base combfilter files

---
+++ src/inc/CombFilter.cpp
@@ -0,0 +1,14 @@
+//
+//  CombFilter.cpp
+//  MUSI6106
+//
+//  Created by JOHN MCNAMARA on 2/10/22.
+//
+#include <cassert>
+#include <iostream>
+
+
+#include "CombFilter.hpp"
+#include "RingBuffer.h"
+#include "AudioFileIf.h"
+#include "Util.h"
---
+++ src/inc/CombFilter.hpp
@@ -0,0 +1,29 @@
+//
+//  CombFilter.hpp
+//  MUSI6106
+//
+//  Created by JOHN MCNAMARA on 2/10/22.
+//
+
+#ifndef CombFilter_hpp
+#define CombFilter_hpp
+
+#include <stdio.h>
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+class CCombFilter{
+    
+};
+
+class CFIRFilter{
+    
+};
+
+class CIIRFilter{
+    
+};
+
+#endif /* CombFilter_hpp */
